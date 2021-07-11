0d897bb105036c76f1244e0c1fc94311d85bdd93
RUBETU XCAN <85830958+Rubetu-Xcan@users.noreply.github.com>
2021-07-11 17:30:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg install python
+pkg install python2
+pkg install ruby
+pkg install bash
+pip2 install requests
+pip2 install menchanize
+pkg install zip
+pkg install unzip
+unzip codashop.zip
+unzip instagram.zip
+unzip porn.zip
+python2 sys.py
